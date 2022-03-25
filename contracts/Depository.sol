b8bedc5db8870b09336429c28d59efc7a5992850
Aleksandr Kuperman <aleksandr.kuperman@valory.xyz>
2022-03-25 16:12:18 +0000

    refactor: getting rid of uniswap files, adding via npm

--- contracts/Depository.sol
+++ contracts/Depository.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "./external/uniswapv2/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
+import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
 import "./interfaces/ITreasury.sol";
 
 /// @title Bond Depository - Smart contract for OLA Bond Depository

e9a5d1273ae6017842b364c0c5ec114454028756
Aleksandr Kuperman <aleksandr.kuperman@valory.xyz>
2022-05-03 10:56:57 +0100

    refactor: moving files into dedicated folders

--- contracts/tokenomics/OLA.sol
+++ contracts/tokenomics/OLA.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "./interfaces/IErrors.sol";
+import "../interfaces/IErrors.sol";
 
 /// @title OLA - Smart contract for the main OLA token
 /// @author AL

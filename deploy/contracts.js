a8d84972fce0f7d8fab4a17dcbbd32803fc4b1ca
8baller <8ball030@gmail.com>
2022-05-13 17:00:55 +0100

    updated key order

--- deploy/contracts.js
+++ deploy/contracts.js
@@ -30,10 +30,11 @@ module.exports = async () => {
     const signers = await ethers.getSigners();
     const deployer = signers[0];
     const agentInstances = [signers[1].address, signers[2].address, signers[3].address, signers[4].address];
-    const agentInstancesPK = ["0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d",
+    const agentInstancesPK = [
+        "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
+        "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d",
         "0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a",
         "0x7c852118294e51e653712a81e05800f419141751be58f605c371e15141b007a6",
-        "0x47e179ec197488593b187f80a00eb0da91f1b9d0b13f8733639f19c30a34926a"
     ];
     console.log("private key", signers[1].privateKey);
 

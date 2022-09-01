36acc656a516b34a15980c02c92250f2cab58487
Aleksandr Kuperman <aleksandr.kuperman@valory.xyz>
2022-09-01 16:29:03 +0100

    chore: adding updated multisig implementation addresses to the deployment script

---
+++ contracts/AutonolasRegistriesMultisig.sol
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+// Importing a different multisig implementation
+import "../lib/autonolas-registries/contracts/multisigs/GnosisSafeSameAddressMultisig.sol";

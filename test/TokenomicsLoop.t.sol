b3ef2c5191b8e53e4a752d16b2c3a8aa75a06317
Aleksandr Kuperman <aleksandr.kuperman@valory.xyz>
2023-05-22 16:32:56 +0100

    chore: updates related to donator veOLAS check in tokenomics

--- test/TokenomicsLoop.t.sol
+++ test/TokenomicsLoop.t.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.8.18;
+pragma solidity =0.8.20;
 
 import {GnosisSafe} from "@gnosis.pm/safe-contracts/contracts/GnosisSafe.sol";
 import {GnosisSafeProxyFactory} from "@gnosis.pm/safe-contracts/contracts/proxies/GnosisSafeProxyFactory.sol";
